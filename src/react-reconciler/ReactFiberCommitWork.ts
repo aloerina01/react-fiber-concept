f5d80044c2ad1a3f40c2cca898ee0122abc1af19
aloerina01 <kiss_mint27@yahoo.co.jp>
2019-02-03 00:04:08 +0900

    Implement commitLifeCycles function

--- src/react-reconciler/ReactFiberCommitWork.ts
+++ src/react-reconciler/ReactFiberCommitWork.ts
@@ -1,5 +1,20 @@
+import { Fiber } from '@/react-reconciler/ReactFiber';
+import { FunctionComponent, ClassComponent } from '@/shared/ReactWorkTags';
+
 export function commitHookEffectList() {}
 
-export function commitLifeCycles() {}
+export function commitLifeCycles(finishedWork: Fiber): void {
+  switch (finishedWork.tag) {
+    case FunctionComponent:
+      commitHookEffectList();
+      break;
+    case ClassComponent:
+      const instance = finishedWork.stateNode;
+      instance.componentDidMount();
+      break;
+    default:
+      break;
+  }
+}
 
 export function commitBeforeMutationLifeCycles() {}
---
+++ src/shared/ReactWorkTags.ts
@@ -0,0 +1,5 @@
+export const FunctionComponent = 0;
+export const ClassComponent = 1;
+
+export type WorkTag = 0 | 1;
+// 他のTypeは省略
